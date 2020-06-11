d231f257c4c63aaca7b1d032512df40999df9925
Rage32 <highgrade89@icloud.com>
2020-06-11 11:37:50 +0300

    DTO classes

---
+++ src/main/java/com/example/linkshrink/dto/WeblinkRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.linkshrink.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Validated
+public class WeblinkRequestDto {
+
+    private String shrinkedLink;
+
+}
---
+++ src/main/java/com/example/linkshrink/dto/WeblinkResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.linkshrink.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeblinkResponseDto {
+    private String fullLInk;
+}
