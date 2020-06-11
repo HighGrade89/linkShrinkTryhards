0e07c36c8036c36df1a1938e70efee9260130738
Rage32 <highgrade89@icloud.com>
2020-06-11 11:39:52 +0300

    Mapper register

---
+++ src/main/java/com/example/linkshrink/mapper/AbstractMapperRegister.java
@@ -0,0 +1,4 @@
+package com.example.linkshrink.mapper;
+
+public abstract class AbstractMapperRegister implements MapperRegister {
+}
---
+++ src/main/java/com/example/linkshrink/mapper/MapperRegister.java
@@ -0,0 +1,7 @@
+package com.example.linkshrink.mapper;
+
+import ma.glasnost.orika.MapperFactory;
+
+public interface MapperRegister {
+    void register(MapperFactory mapperFactory);
+}
