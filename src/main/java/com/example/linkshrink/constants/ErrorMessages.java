40e1035525b331ae0064e430d063891e1e1beb75
Rage32 <highgrade89@icloud.com>
2020-06-14 02:19:05 +0300

    Minor ui changes

--- src/main/java/com/example/linkshrink/constants/ErrorMessages.java
+++ src/main/java/com/example/linkshrink/constants/ErrorMessages.java
@@ -3,11 +3,11 @@ package com.example.linkshrink.constants;
 public class ErrorMessages {
 
     public static final String GENERAL_ERROR = "[LINKSHRINK-E001] General error occured";
+    public static final String INBOUND_URL_INVALID = "[LINKSHRINK-E001] Ivalid inbound url";
     public static final String URL_NOT_FOUND = "[LINKSHRINK-E002] Url was not found";
-    public static final String INBOUND_URL_INVALID = "[LINKSHRINK-E003] Ivalid inbound url";
+    public static final String URL_HAS_EXPIRED = "[LINKSHRINK-E003] Url has expired";
 
-    private  ErrorMessages() {
 
-    }
+    private  ErrorMessages() {}
 }
 
