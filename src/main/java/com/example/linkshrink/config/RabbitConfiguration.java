a06cf63e654074245f10e9ef5ffcd2bfe819ce3a
rage32 <highgrade89@icloud.com>
2020-06-18 05:49:10 +0300

    Updated rabbit configuration

--- src/main/java/com/example/linkshrink/config/RabbitConfiguration.java
+++ src/main/java/com/example/linkshrink/config/RabbitConfiguration.java
@@ -1,17 +1,16 @@
 package com.example.linkshrink.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class RabbitConfiguration {
@@ -19,9 +18,7 @@ public class RabbitConfiguration {
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
-        return connectionFactory;
+        return new CachingConnectionFactory("localhost");
     }
 
     @Bean
@@ -31,14 +28,7 @@ public class RabbitConfiguration {
 
     @Bean
     public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rt = new RabbitTemplate(connectionFactory());
-        return rt;
-    }
-
-    @Bean
-    public Queue myQueue1() {
-        Queue q = new Queue("q1");
-        return q;
+        return new RabbitTemplate(connectionFactory());
     }
 
 }
